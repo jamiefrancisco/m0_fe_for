8afe7f6b35b59953be33af6daea1062a7cfca46c
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-09-21 16:27:54 -0700

    Write code for the three For Statement challenges

--- for.js
+++ for.js
@@ -3,18 +3,31 @@
   
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-
+for (i = 25; i <= 75; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
 
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
-var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+var weatherOptions = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+
+for (i = 0; i < weatherOptions.length; i++) {
+  console.log(`The weather today is:  ${weatherOptions[i]}`);
+}
 
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+for (i = 0; i < words.length; i++) {
+  if (words[i].length === 3) {
+    console.log(words[i]);
+  }
+}
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
   // Use technical vocabulary and explain each part to demonstrate your understanding! 
